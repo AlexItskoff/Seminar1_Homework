5a822985329cda2b7ccec1cdd0c00c0d9e142523
AlexItskoff <119017869+AlexItskoff@users.noreply.github.com>
2022-12-02 20:21:43 +0300

    Add files via upload

---
+++ Test_2.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int numberA = new Random().Next(-1000, 1000);    // -999  -998...998  999
+Console.WriteLine(numberA);
+int numberB = new Random().Next(-1000, 1000);
+Console.WriteLine(numberB);
+int result = (numberA - numberB);
+//Console.WriteLine(result);
+
+if (result>0) Console.WriteLine(numberA);
+if (result<0) Console.WriteLine(numberB);
+if (result==0) Console.WriteLine("Числа равны");
+
+
+    
+
+
+
+
---
+++ Test_8.cs
@@ -0,0 +1,22 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int numberA = new Random().Next(1, 30);    // 1  2 ... 30
+Console.WriteLine("четные числа от 1 до "  +numberA);
+
+int counter = 1 ;
+
+
+while (counter<=numberA)
+{
+   if (counter%2==0) Console.WriteLine(counter);
+   counter = counter + 1 ;
+}
+
+
+
+
+
